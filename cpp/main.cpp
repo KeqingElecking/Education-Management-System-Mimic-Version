755bb67f858416d4ad1fd39fcc7956758698e041
yokubari <mjnhpzo2004@gmail.com>
2024-06-01 17:33:59 +0700

    Split code into a header file and a source file

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,57 +8,8 @@
 
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include "sinhvien.hpp"
 using namespace std;
-
-class SinhVien {
-private:
-    string mssv;
-    string hoTen;
-    unordered_map<string, float> grades;
-    float diemMonHoc[2];
-
-public:
-    string getMSSV() {
-        return mssv;
-    }
-
-    void nhapThongTin() {
-        cout << "---Thong Tin Sinh vien---\n";
-        cout << "Nhap mssv: ";
-        cin >> mssv;
-        cout << "Nhap ho ten: ";
-        cin.ignore();
-        getline(cin, hoTen);
-        // Cần thêm thông tin điểm cho từng môn học sử dụng map
-        cout << "Nhap diem qua trinh: ";
-        cin >> diemMonHoc[0];
-        cout << "Nhap diem cuoi ky: ";
-        cin >> diemMonHoc[1];
-    }
-
-    void hienThiThongTin(vector<SinhVien>& danhSachSinhVien) {
-        cout << "---Danh Sach Sinh vien---\n";
-        for (auto sv : danhSachSinhVien) {
-            cout << "Ma sinh vien: " << mssv << endl
-                << "Ho ten: " << hoTen << endl
-                << "Mon hoc: " << endl
-                << "    QT: " << diemMonHoc[0] << endl
-                << "    CK: " << diemMonHoc[1] << endl;
-            cout << endl;
-        }
-        system("pause");
-    }
-
-    void suaThongTin(vector<SinhVien>& danhSachSinhVien) {
-        //... them code sua thong tin sinh vien
-    }
-
-
-    void xoaThongTin(vector<SinhVien>& danhSachSinhVien) {
-        //... them code xoa thong tin sinh vien
-    }
-};
 
 int main() {
     vector<SinhVien> danhSachSinhVien;

8b9ba4c48c1026609b5fd60053ff63ef23cf30ca
schizoneko <viettung11122004@gmail.com>
2024-06-10 19:19:13 +0700

    UPDATE version 2.0 c

---
+++ c_ver2/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include "sinhvien.h"
+
+int main() {
+	struct SinhVien dssv[100];
+	int slsv = 0;
+	int luaChon;
+	
+	// docFile(dssv, &slsv);
+	printf("DANH SACH SINH VIEN HIEN TAI:\n");
+	hienThiDSSV(dssv, slsv);
+	int i;
+				
+	do {
+		printf("=============== MENU ===============");
+		printf("\n1. Them Sinh vien moi");
+		printf("\n2. Hien thi danh sach");
+		printf("\n3. Sap xep theo ten A-Z");
+		printf("\n4. Sap xep theo diem giam dan");
+		printf("\n5. Tim sinh vien theo ten");
+		printf("\n6. Xuat danh sach ra file .txt");
+		printf("\n\n0. Thoat");
+		printf("\nOption: ");
+		
+		scanf("%d", &luaChon);
+		struct SinhVien sv;
+		
+		switch(luaChon) {
+			case 0:
+				break;
+				
+			case 1:
+				sv = nhapSV();
+				dssv[slsv++] = sv;
+				break;
+				
+			case 2:
+				hienThiDSSV(dssv, slsv);
+				break;
+				
+			case 3:
+				sapXepTheoTen(dssv, slsv);
+				printf("\nDanh sach sinh vien sau khi sap xep theo ten a-z:\n");
+				hienThiDSSV(dssv, slsv);
+				break;
+				
+			case 4:
+				sapXepTheoDiem(dssv, slsv);
+				printf("\nDanh sach sinh vien sau khi sap xep theo diem giam dan:\n");
+				hienThiDSSV(dssv, slsv);
+				break;
+				
+			case 5:
+				timTheoTen(dssv, slsv);
+				break;
+				
+			case 6:
+				ghiFile(dssv, slsv);
+				break;
+				
+			default:
+				printf("Sai chuc nang, vui long chon lai!\n");
+				break;
+		}
+		
+	} while(luaChon);
+	
+	return 0;
+}
---
+++ c_ver2/sinhvien.h
@@ -0,0 +1,232 @@
+#include <stdio.h>
+#include <string.h>
+
+struct SinhVien;
+void hienThiDSSV(struct SinhVien*, int);
+void hienThiTenCot();
+
+struct HoTen {
+	char ho[20];
+	char ten[40];
+};
+
+struct DiemMH {
+	float QT;
+	float CK;
+	float DiemTB;
+	char DiemHocPhan[2];
+};
+
+struct SinhVien {
+	int mssv;
+	struct HoTen hoVaTen;
+	char Lop[10];
+	char gioiTinh[10];
+	struct DiemMH diem;
+};
+
+/**
+ * @brief Nhap diem qua trinh va cuoi ky, tinh diem trung binh va diem hoc phan
+ * 
+ * @param diem 
+ */
+void nhapDiem(struct DiemMH* diem) {
+	printf("QT: ");
+	scanf("%f", &diem->QT);
+	printf("CK: ");
+	scanf("%f", &diem->CK);
+	printf("TB: ");
+	diem->DiemTB = (diem->QT*0.3 + diem->CK*0.7);
+    if(diem->DiemTB >= 9) {
+        strcpy(diem->DiemHocPhan, "A");
+    } else if(diem->DiemTB >= 8) {
+        strcpy(diem->DiemHocPhan, "B");
+    } else if(diem->DiemTB >= 7) {
+        strcpy(diem->DiemHocPhan, "C");
+    } else if(diem->DiemTB >= 6) {
+        strcpy(diem->DiemHocPhan, "D");
+    } else if(diem->DiemTB >= 5) {
+        strcpy(diem->DiemHocPhan, "E");
+    } else {
+        strcpy(diem->DiemHocPhan, "F");
+    }
+}
+
+/**
+ * @brief Nhap ho va ten sinh vien
+ * 
+ * @param ten 
+ */
+void nhapHoTen(struct HoTen* ten) {
+	printf("Ho: ");
+	scanf("%s", ten->ho);
+	printf("Ten (VD:Viet_Tung): ");
+	scanf("%s", ten->ten);
+}
+
+/**
+ * @brief Nhap thong tin sinh vien
+ * 
+ * @return struct SinhVien 
+ */
+struct SinhVien nhapSV() {
+	struct SinhVien sv;
+	printf("Nhap mssv: ");
+	scanf("%d", &sv.mssv);
+	nhapHoTen(&sv.hoVaTen);
+	printf("Lop (VD:ET-E9_01): ");
+	scanf("%s", &sv.Lop);
+	printf("Gioi tinh: ");
+	scanf("%s", &sv.gioiTinh);
+	nhapDiem(&sv.diem);
+	return sv;
+}
+
+/**
+ * @brief Hien thi thong tin sinh vien
+ * 
+ * @param sv 
+ */
+void hienThiTTSV(struct SinhVien sv) {
+	printf("%-10d %-10s %-20s %-10s %-10s %-10.2f %-10.2f %-10.2f %-10s\n",
+	sv.mssv, sv.hoVaTen.ho, sv.hoVaTen.ten, sv.Lop, sv.gioiTinh,
+	sv.diem.QT, sv.diem.CK, sv.diem.DiemTB, sv.diem.DiemHocPhan);
+}
+
+/**
+ * @brief Sap xep danh sach sinh vien theo chu cai dau cua ten
+ * 
+ * @param ds 
+ * @param slsv 
+ */
+void sapXepTheoTen(struct SinhVien* ds, int slsv) {
+	int i, j;
+	for(i = 0; i < slsv - 1; i++) {
+		for(j = slsv - 1; j > i; j --) {
+			if(strcmp(ds[j].hoVaTen.ten, ds[j-1].hoVaTen.ten) < 0) {
+				struct SinhVien sv = ds[j];
+				ds[j] = ds[j - 1];
+				ds[j - 1] = sv;
+			}
+		}
+	}
+}
+
+/**
+ * @brief Sap xep danh sach sinh vien theo diem trung binh giam dan
+ * 
+ * @param ds 
+ * @param slsv 
+ */
+void sapXepTheoDiem(struct SinhVien* ds, int slsv) {
+	int i, j;
+	for(i = 0; i < slsv - 1; i++) {
+		for(j = slsv - 1; j > i; j --) {
+			if(ds[j].diem.DiemTB > ds[j - 1].diem.DiemTB) {
+				struct SinhVien sv = ds[j];
+				ds[j] = ds[j - 1];
+				ds[j - 1] = sv;
+			}
+		}
+	}
+}
+
+/**
+ * @brief Tim sinh vien theo ten
+ * 
+ * @param ds 
+ * @param slsv 
+ */
+void timTheoTen(struct SinhVien* ds, int slsv) {
+	char ten[20];
+	printf("Nhap ten: ");
+	scanf("%s", ten);
+	hienThiTenCot();
+	int i, timSV = 0;
+	for(i = 0; i < slsv; i++) {
+		if(strcmp(ten, ds[i].hoVaTen.ten) == 0) {
+			hienThiTTSV(ds[i]);
+			timSV = 1;
+		}
+	}
+	if(timSV == 0) {
+		printf("Khong co sinh vien %s trong danh sach!\n", ten);
+	}
+}
+
+/**
+ * @brief Ghi danh sach sinh vien ra file .txt
+ * 
+ * @param ds 
+ * @param slsv 
+ */
+void ghiFile(struct SinhVien* ds, int slsv) {
+	getchar();
+	char fName[26];
+	printf("Nhap ten file: ");
+	gets(fName);
+	FILE* fOut = fopen(fName, "a");
+	int i;
+	for(i = 0; i < slsv; i++) {
+		struct SinhVien sv = ds[i];
+		fprintf(fOut, "%-10d %-10s %-20s %-10s %-10s %-10.2f %-10.2f %-10.2f %-10s\n",
+		sv.mssv, sv.hoVaTen.ho, sv.hoVaTen.ten, sv.Lop, sv.gioiTinh,
+		sv.diem.QT, sv.diem.CK, sv.diem.DiemTB, sv.diem.DiemHocPhan);
+	}
+	fclose(fOut);
+}
+
+/**
+ * @brief Doc danh sach sinh vien tu file .txt
+ * 
+ * @param ds 
+ * @param slsv 
+ */
+void docFile(struct SinhVien* ds, int* slsv) {
+	FILE* fOut = fopen("SV.txt", "r");
+	int i = 0;
+	if(fOut) {
+		for(;;) {
+			struct SinhVien sv;
+			fscanf(fOut, "%10d %10s %20[^\n] %10s %10s %10f %10f %10f %10s\n",
+			&sv.mssv, sv.hoVaTen.ho, sv.hoVaTen.ten, &sv.Lop, sv.gioiTinh,
+			&sv.diem.QT, &sv.diem.CK, &sv.diem.DiemTB, sv.diem.DiemHocPhan);
+			
+			ds[i++] = sv;
+			if(feof(fOut)) { // thoat chuong trinh
+				break;
+			}
+		}
+	}
+	
+	fclose(fOut);
+	*slsv = i;
+}
+
+/**
+ * @brief Hien thi hang dau tien cua bang danh sach sinh vien
+ * 
+ */
+void hienThiTenCot() {
+	printf("-----------------------------------------------------"
+	"----------------------------------------------------------------------------\n");
+	printf("%-10s %-10s %-20s %-10s %-10s %-10s %-10s %-10s %-12s \n", 
+		"Mssv", "Ho", "Ten", "Lop", "Gioi Tinh", 
+		"Diem QT", "Diem CK", "Diem TB", "DiemHocPhan");
+}
+
+/**
+ * @brief Hien thi danh sach cua tat ca sinh vien hien tai
+ * 
+ * @param ds 
+ * @param slsv 
+ */
+void hienThiDSSV(struct SinhVien* ds, int slsv) {
+	hienThiTenCot();
+	int i;
+	for(i = 0; i < slsv; i++) {
+		hienThiTTSV(ds[i]);
+	}
+	printf("-----------------------------------------------------"
+	"----------------------------------------------------------------------------\n");
+}

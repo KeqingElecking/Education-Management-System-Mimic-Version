9d1776fae9eab10e50c1f1b347e4896adc6720eb
schizoneko <viettung11122004@gmail.com>
2024-06-19 17:43:24 +0700

    TEST: add new student.c file in c_ver2, fix c ver 1 #include not found bug

--- c/lib/student.h
+++ c/lib/student.h
@@ -15,5 +15,9 @@ typedef struct {
 void initStudent(Student *student, const char *id, const char *name);
 void addCourse(Student *student, const Course *course);
 void displayStudent(const Student *student);
+/*int findCourse(Student* student, const char* courseName);
+void editCourse(Student* student, const char* courseName, float midterm, float final);
+void removeCourse(Student* student, const char* courseName);
+void displayCourses(Student* student);*/
 
 #endif
